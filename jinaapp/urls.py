e997ab40f4203d7592a8da750bb972e4fd9ec3ba
mmardini <mardini_mohammad@hotmail.com>
2024-01-24 19:56:46 +0300

    Add files via upload

---
+++ jinaapp/urls.py
@@ -0,0 +1,7 @@
+# yourappname/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
---
+++ jinaapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+# yourappname/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'jinaapp/index.html')
